e71543a229e4333c11611ed3ecd06698b7dfafd4
pandavinci <ultimatepandamanqq@gmail.com>
2025-04-22 14:54:46 +0200

    removed a number of speakers check

--- train_and_eval.py
+++ train_and_eval.py
@@ -22,9 +22,6 @@ def main():
     )
 
     model, trainer = build_model(args, num_classes=len(np.bincount(train_dataloader.dataset.get_labels())))
-    print(f"Number of speakers: {train_dataloader.dataset.get_num_speakers()}")
-    exit()
-
     # TODO: Implement training of MHFA and AASIST with SkLearn models
 
     print(f"Training on {type(train_dataloader.dataset).__name__} dataloader.")
