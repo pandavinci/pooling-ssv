6f1e60e5dacc5655451d5dfc44e59714dfab3301
pandavinci <ultimatepandamanqq@gmail.com>
2025-07-01 16:59:38 +0200

    resnet itself, with mel-spectrogram as the feature extractor

--- extractors/MelSpectrogram.py
+++ extractors/MelSpectrogram.py
@@ -1,12 +1,17 @@
 import torchaudio
 import torch.nn as nn
+import torch
 
 class MelSpectrogram(nn.Module):
-    def __init__(self, n_fft=2048, hop_length=512, n_mels=128):
+    def __init__(self, n_fft=2048, hop_length=512, n_mels=128, device="cuda" if torch.cuda.is_available() else "cpu"):
         super(MelSpectrogram, self).__init__()
         self.n_fft = n_fft
         self.hop_length = hop_length
         self.feature_size = n_mels
+        self.device = device
+        self.transform = torchaudio.transforms.MelSpectrogram(n_fft=self.n_fft, hop_length=self.hop_length, n_mels=self.feature_size).to(device)
 
     def extract_features(self, x):
-        return torchaudio.transforms.MelSpectrogram(n_fft=self.n_fft, hop_length=self.hop_length, n_mels=self.feature_size)(x)
+        x = self.transform(x)
+        x = torch.permute(x, (0, 2, 1)) # (batch_size, n_time, n_mels)
+        return x
--- feature_processors/ResNet.py
+++ feature_processors/ResNet.py
@@ -1,20 +1,22 @@
-import wespeaker
+from wespeaker.models.resnet import ResNet293 as build_ResNet293
 from feature_processors.BaseProcessor import BaseProcessor
 import torch.nn as nn
 
 
 class ResNet293(nn.Module, BaseProcessor):
-    def __init__(self, input_dim=1024, output_dim=1024, pooling_func='TSTP', two_emb_layer=False):
+    def __init__(self, input_dim=128, output_dim=128, pooling_func='TSTP', two_emb_layer=False):
         super(ResNet293, self).__init__()
         self.input_dim = input_dim
         self.output_dim = output_dim
 
-        self.model = wespeaker.models.ResNet293(
+        self.model = build_ResNet293(
             feat_dim=input_dim,
             embed_dim=output_dim,
             pooling_func=pooling_func,
             two_emb_layer=two_emb_layer
         )
+        print(self.model)
 
     def forward(self, x):
+        print(x.shape)
         return self.model(x)
